d937f3452784d8945c13c2e6ee44d64023f963ea
Gangeswara <jjgangeswara@gmail.com>
2026-05-12 19:03:11 +0530

    Solved Two Sum using HashMap
    Implement Two Sum solution using HashMap.

---
+++ Arrays/two-sum.java
@@ -0,0 +1,34 @@
+/*
+Problem: Two Sum
+
+Approach:
+1. Use HashMap
+2. Store number and index
+3. Check target - current element
+
+Time Complexity: O(n)
+Space Complexity: O(n)
+*/
+
+import java.util.HashMap;
+
+class Solution {
+
+    public int[] twoSum(int[] nums, int target) {
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for(int i = 0; i < nums.length; i++) {
+
+            int diff = target - nums[i];
+
+            if(map.containsKey(diff)) {
+                return new int[]{map.get(diff), i};
+            }
+
+            map.put(nums[i], i);
+        }
+
+        return new int[]{};
+    }
+}
